e8fb037975f7ec0cb4b02e4716737e7227d19df6
amja <me@amosjackson.com>
2020-07-16 00:32:55 +0100

    Fix 2014 site links

--- 2013/header.php
+++ 2013/header.php
@@ -23,12 +23,12 @@
 		<h1>amos jackson</h1><br>
 		<h2>est. 1997</h2>
 		<div id="nav">
-			<a class="links" id="homelink" href="index">Home</a>
+			<a class="links" id="homelink" href="index.php">Home</a>
 			<a class="slash">/</a>
-			<a class="links" id="stufflink" href="portfolio">Portfolio</a>
+			<a class="links" id="stufflink" href="portfolio.php">Portfolio</a>
 			<a class="slash">/</a>
-			<a class="links" id="contactlink" href="contact">Contact</a>
+			<a class="links" id="contactlink" href="contact.php">Contact</a>
 			<a class="slash">/</a>
-			<a class="links" id="melink" href="about">About</a></div>
+			<a class="links" id="melink" href="about.php">About</a></div>
 		
 		</header>
